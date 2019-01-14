be62a7a3ab52c5aef2041340082382eebe1080fe
joeyhere <josephodell@outlook.com>
2019-01-14 14:26:54 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,6 +13,10 @@ class Instructor
     @@all
   end
 
+  def tests
+    BoatingTest.all.select {|test| test.instructor == self}
+  end
+
   def pass_student(student_name, test_name)
     existing_test = BoatingTest.all.find{|test| test.student.name == student_name && test.test_name == test_name}
     if existing_test
