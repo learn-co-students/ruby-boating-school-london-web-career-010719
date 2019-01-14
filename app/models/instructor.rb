6f0a472c4de23a82d6063b8b03ba5af9d587528b
studioshane <studioshane@gmail.com>
2019-01-14 15:40:59 +0000

    All methods running

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,44 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def my_tests
+    BoatingTest.all.select do |test|
+      test.instructor == self
+    end
+  end
+
+  def pass_student(student, test_name, test_status="passed")
+        if my_tests.include?(Student.name == student && BoatingTest.name == test_name)
+        test_name.test_status = "passed"
+        return test
+      else
+        BoatingTest.new(student, test_name, test_status, self)
+      end
+  end
+
+  def fail_student
+    def pass_student(student, test_name, test_status="failed")
+          if my_tests.include?(Student.name == student && BoatingTest.name == test_name)
+          test_name.test_status = "failed"
+          return test
+        else
+          BoatingTest.new(student, test_name, test_status, self)
+        end
+    end
+
+
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,37 @@
 class Student
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def self.find_student(name)
+    all.select {|student|
+      student.name == name.capitalize}
+  end
+
+  def tests_taken
+    BoatingTest.all.select do |test|
+      test.student == self
+    end
+  end
+
+  def grade_percentage
+    i = tests_taken.select { |test|
+      test.test_status == "passed"}.length.to_f/tests_taken.length.to_f
+      i*100
+  end
+
 end
